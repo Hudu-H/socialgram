bbee2e40fb3d2907bc7c7c2ea1d3c1724269a3c9
Hudu <ahmedhudu21@gmail.com>
2024-12-06 18:32:55 +0100

    implement edit profile

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUser } from "@/lib/react-query/queriesAndMutations";
 
 const LikedPosts = () => {
-  return <div>LikedPosts</div>;
+  const { data: currentUser } = useGetCurrentUser();
+
+  if (!currentUser)
+    return (
+      <div className="h-full w-full flex-center">
+        <Loader />
+      </div>
+    );
+
+  return (
+    <>
+      {currentUser.liked.length === 0 && (
+        <p className="text-light-3">No liked posts</p>
+      )}
+
+      <GridPostList posts={currentUser.liked} showStats={false} />
+    </>
+  );
 };
 
 export default LikedPosts;
